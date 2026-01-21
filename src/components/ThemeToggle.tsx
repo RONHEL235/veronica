a83e027a5eec5133eb2d2f0c7e8b9a5c247b67e3
Ronee <137155067+RONHEL235@users.noreply.github.com>
2026-01-21 23:56:53 +0200

    This is where it all starts

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -5,6 +5,7 @@ import { useTheme } from "../hooks/useTheme";
 export default function ThemeToggle() {
   const { theme, toggleTheme } = useTheme();
 
+  //I think this is where the problem starts 
   return (
     <button
       onClick={toggleTheme}
